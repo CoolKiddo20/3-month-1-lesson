aa1052da4f1e596969125be92e9ebd35d49d1f17
CoolKiddo20 <161250160+CoolKiddo20@users.noreply.github.com>
2024-05-06 10:17:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const userName = prompt("Enter your name: ");
+const userSurname = prompt("Enter your surname: ");
+const userAge = prompt("Enter your age: ");
+const userSubject = prompt("Enter your favourite subject: ");
+const userRegion = prompt("Enter your region: ");
+
+
+
+console.log(
+    `Name: ${userName}
+    Surname: ${userSurname}
+    Age: ${userAge}
+    Favourite subject: ${userSubject}
+    Region: ${userRegion}
+`);
